6beaac186070a6ec0138589f07df7dd35b9788b9
Richarde Lira <richarde-ips@outlook.com>
2022-10-08 20:42:05 -0300

    Creating the model file and starting the train of the model

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
-
+from model import NeuralNet
 
 with open("intents.json", 'r') as f:
     intents = json.load(f)
@@ -36,8 +36,6 @@ tags      = sorted(set(tags))
 
 # Separate the data in Train
 
-print(tags)
-
 X_train = []
 y_train = []
 
@@ -65,21 +63,46 @@ class ChatDataset(Dataset):
         return self.X_data[index], self.y_data[index]
 
 
-    def _len_(self):
+    def __len__(self):
         return self.n_samples
 
 # Hyperparameters 
 
-batch_size = 8
+batch_size  = 8
+hidden_size = 8
+output_size = len(tags)
+input_size  = len(X_train[0])
+learning_rate = 0.001
+num_epochs = 1000
+
 
 dataset = ChatDataset()
 train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True,num_workers=2)
 
+# Check if the GPU is avaible 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
 
+# loss and optimizer 
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, )
 
+for epoch in range(num_epochs):
+    for(words, labels) in train_loader:
+        words  = words.to(device)
+        labels = labels.to(device)
 
+        # forward
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+        # Backward and optimezer setp
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    
+    if (epoch +1) % 100 == 0:
+        print(f'epoch {epoch+1}/{num_epochs}, loss= {loss.item():.4f}')
 
+print(f'final loss, loss= {loss.item():.4f}')
 
-
-        
